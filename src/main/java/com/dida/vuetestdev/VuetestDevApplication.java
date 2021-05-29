3c1b454aa6d1d926d3aaae5656cf520bb2a98104
PandyYang <yangpandy@gmail.com>
2021-05-29 18:55:18 +0800

    Initial commit

---
+++ src/main/java/com/dida/vuetestdev/VuetestDevApplication.java
@@ -0,0 +1,13 @@
+package com.dida.vuetestdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuetestDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuetestDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dida/vuetestdev/VuetestDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dida.vuetestdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuetestDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
